65c0382917e0cf408ab64c52ba2336e194e62522
Joshua Cheung <contact.cjoshua@gmail.com>
2022-06-02 17:59:40 -0700

    complete addProperty function

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -136,7 +136,7 @@ const getAllProperties = function (options, limit = 10) {
     queryParams.push(`${options.minimum_price_per_night}00`);
     queryString += `AND cost_per_night BETWEEN $${queryParams.length} `;
     queryParams.push(`${options.maximum_price_per_night}00`);
-    queryString += `AND $${queryParams.length}`;
+    queryString += `AND $${queryParams.length} `;
   }
   // If user input minimum rating
   if (options.minimum_rating) {
@@ -154,7 +154,6 @@ const getAllProperties = function (options, limit = 10) {
   `;
 
   // 5
-  console.log("query:", queryString, "para:", queryParams);
 
   // 6
   return pool.query(queryString, queryParams).then((res) => res.rows);
@@ -167,9 +166,49 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  console.log("property", property);
+  const queryString = `
+  INSERT INTO properties 
+  (owner_id,
+    title,
+    description,
+    thumbnail_photo_url,
+    cover_photo_url,
+    cost_per_night,
+    street,
+    city,
+    province,
+    post_code,
+    country,
+    parking_spaces,
+    number_of_bathrooms,
+    number_of_bedrooms)
+  VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14) RETURNING *;
+  `;
+
+  const queryParams = [
+    property.owner_id,
+    property.title,
+    property.description,
+    property.thumbnail_photo_url,
+    property.cover_photo_url,
+    property.cost_per_night,
+    property.street,
+    property.city,
+    property.province,
+    property.post_code,
+    property.country,
+    property.parking_spaces,
+    property.number_of_bathrooms,
+    property.number_of_bedrooms,
+  ];
+
+  console.log("query:", queryString, "para:", queryParams);
+  return pool
+    .query(queryString, queryParams)
+    .then((data) => data.rows)
+    .catch((err) => {
+      console.log(err);
+    });
 };
 exports.addProperty = addProperty;
